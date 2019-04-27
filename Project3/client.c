dab4fc21e655c44f0c93fa79d2c058848b61c69a
Maximilian Orozco <maxorozco213@gmail.com>
2019-04-26 21:27:45 -0700

    Finished number 1

--- Project3/client.c
+++ Project3/client.c
@@ -9,38 +9,66 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[0]) {
   int createSocket = 0, n = 0;
-  char receiveString[1024];
+  int connectStatus;
+  char receiveString[256];
+  char* sendString;
+  bool run = true;
   struct sockaddr_in serverIP;
   memset(receiveString, '0', sizeof(receiveString));
+
+  //check inputted string from cmd line
+  if(argc == 2) {
+    printf("Sent string: %s\n", argv[1]);
+
+    sendString = argv[1];
+  }
+  else if(argc > 2) {
+    printf("Too many arguments.\n");
+    exit(0);
+  }
+  else {
+    printf("One string expected.\n");
+    exit(0);
+  }
 
   if((createSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("Socket not created... \n");
     return 1;
   }
 
+  //create socket structure
   serverIP.sin_family = AF_INET;
   serverIP.sin_port = htons(2017);
   serverIP.sin_addr.s_addr = inet_addr("127.0.0.1");
+  //connect to the server
+  connectStatus = connect(createSocket, (struct sockaddr *)&serverIP, sizeof(serverIP));
 
-  if(connect(createSocket, (struct sockaddr *)&serverIP, sizeof(serverIP)) < 0) {
-    printf("Connection failed...");
+  //check the status of the connection
+  if(connectStatus < 0) {
+    printf("Connection failed...\n");
     return 1;
   }
+  else {
+    send(createSocket, sendString, sizeof(sendString), 0);
+  }
 
+  //read the response from the server
   while((n = read(createSocket, receiveString, sizeof(receiveString) - 1)) > 0) {
+    printf("Returned: ");
     receiveString[n] = 0;
     if(fputs(receiveString, stdout) == EOF) {
-      printf("\nStandard output error");
+      printf("\nStandard output error\n");
     }
 
     printf("\n");
   }
 
   if(n < 0){
-    printf("Standard input error.");
+    printf("Standard input error.\n");
   }
 
   return 0;
--- Project3/server.c
+++ Project3/server.c
@@ -11,9 +11,32 @@
 #include <stdbool.h>
 #include <pthread.h>
 
+//take given string and reverse it
+void* reverseString(void* normalString) {
+  char* str = normalString;
+  char temp;
+  int i, j = 0;
+
+  i=0;
+  j=strlen(str)-1;
+
+  while(i<j){
+    temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+    i++;
+    j--;
+  }
+
+  // str = reverseString;
+  // printf("\nReverse string is: %s\n", str);
+  return NULL;
+}
+
 int main(int argc, char *argv[0]) {
   time_t clock;
-  char string[1025];
+  char string[256];
+  char receiveString[256];
   int clientListen = 0;
   int clientConnect = 0;
   bool run = true;
@@ -25,26 +48,32 @@ int main(int argc, char *argv[0]) {
   memset(&serverIP, '0', sizeof(serverIP));
   memset(string, '0', sizeof(string));
 
+  //create the socket structure
   serverIP.sin_family = AF_INET;
   serverIP.sin_port = htons(2017);
-  serverIP.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverIP.sin_addr.s_addr = INADDR_ANY;
+
   //bind the sockets
   bind(clientListen, (struct sockaddr*)&serverIP, sizeof(serverIP));
-  //listen for requests
+  //listen for requests from the client
   listen(clientListen, 20);
 
+  //run until stopped and accept requests
   while(run) {
     printf("Server running...\n\n");
     //accept an incoming request
     clientConnect = accept(clientListen, (struct sockaddr*) NULL, NULL);
-
-    clock = time(NULL);
-    snprintf(string, sizeof(string), "%.24s\r\n", ctime(&clock));
-    write(clientConnect, string, strlen(string));
-
+    printf("Server hit...\n\n");
+    recv(clientConnect, receiveString, sizeof(receiveString), 0);
+    //create a thread passing in the string from client
+    pthread_create(&request, 0, reverseString, receiveString);
+    //join the thread and get the result
+    pthread_join(request, NULL);
+    //write into a buffer (buffer name, size, format, content)
+    write(clientConnect, receiveString, strlen(receiveString));
+    //close the socket
     close(clientConnect);
     sleep(1);
-    pthread_join(request, NULL);
   }
 
   return 0;
